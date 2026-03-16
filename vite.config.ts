788b8ccc0b84722cef8a3a22aefb4afae0909ad6
hardmrzhang <7452519+hardmrzhang@user.noreply.gitee.com>
2026-03-17 01:16:58 +0800

    **

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'global': 'globalThis',
-  },
+  plugins: [
+    react(),
+    nodePolyfills({ include: ['buffer', 'crypto', 'stream', 'util'] }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
